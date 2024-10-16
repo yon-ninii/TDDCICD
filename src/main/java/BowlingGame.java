c290413de9f324e9fd0df002668d700bc8b57392
yon-ninii <701pooh@naver.com>
2024-10-16 15:59:54 +0900

    asd

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -19,7 +19,7 @@ public class BowlingGame {
 
     public void roll(int pins) {
         if(pins < 0 || pinNow < pins) {
-            throw new IllegalArgumentException("Pins in one frame must be between 0 and 10");
+            throw new IllegalArgumentException("Pins in one frame must be between 0 and 10asd");
         }
 
         totalRollCount++;
